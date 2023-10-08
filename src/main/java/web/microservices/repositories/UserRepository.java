e9b63f9514c2b345fb9c6f662de4b5905002a17b
lelek337 <ioan.af@mail.ru>
2023-10-08 13:33:05 +0300

    This is my solution to this task

---
+++ src/main/java/web/microservices/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package web.microservices.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import web.microservices.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ src/main/java/web/microservices/service/UserService.java
@@ -0,0 +1,44 @@
+package web.microservices.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.microservices.model.User;
+import web.microservices.repositories.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public User findOne (long id) {
+        Optional<User> foundUser = userRepository.findById(id);
+        return foundUser.orElse(null);
+    }
+
+    public void save(User user) {
+    userRepository.save(user);
+    }
+
+    public void update(Long id, User updateUser) {
+        updateUser.setId(id);
+        userRepository.save(updateUser);
+    }
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+}
